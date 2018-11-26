9453dfa03cfc0930c0092426be0f70b95745487c
Sarah Fodor <sarahfodor@Sarahs-MacBook-Pro-3.local>
2018-11-26 17:26:29 -0500

    first function

---
+++ HW-DB.py
@@ -0,0 +1,138 @@
+# Import statements
+import unittest
+import sqlite3
+import requests
+import json
+import re
+import tweepy
+import twitter_info # still need this in the same directory, filled out
+
+## [PART 1]
+# Finish the function get_tweets that searches for all tweets created by the user "umsi" and
+# all tweets that mention "umsi"
+# It takes as input the api object, the cache dictionary, and the cache file name
+# It caches the data and will use the cached data if it exists
+def get_tweets(api, cacheDict, fname):
+
+    # if the data is in the dictionary return it
+    searchTerm = "umsi"
+    if searchTerm in cacheDict:
+        print("Using data from cache")
+
+    # otherwise get the data, add it the dictionary, and write it to a file
+    else:
+        print("Fetching tweets")
+
+        # get tweets by the umsi user
+        umsi_user = api.user_timeline(screen_name = 'umsi', include_rts = True)
+        list_of_tweets = []
+        for tweet in umsi_user:
+            list_of_tweets.append(tweet)
+        # get tweets that mention umsi
+        user_tweets = api.search(q = "umsi")
+        for tweet in user_tweets["statuses"]:
+            list_of_tweets.append(tweet)
+        # add both to the dictionary
+        cacheDict[searchTerm] = list_of_tweets
+        print(len(list_of_tweets))
+        # write out the dictionary as JSON
+        json_dumps = json.dumps(cacheDict)
+        open_cache = open(fname, 'w')
+        open_cache.write(json_dumps)
+        open_cache.close()
+
+    # return the data
+    return cacheDict[searchTerm]
+
+
+## [PART 2]
+#
+# Finish the function setUpTweetTable that takes a list of tweets, a sqlite connection object, and a cursor and inserts the tweet information in the database
+# Fist create a database: tweets.sqlite,
+# Then load all of the tweets into a table called Tweets, with the following columns in each row:
+## tweet_id - containing the unique id that belongs to each tweet
+## author - containing the screen name of the user who posted the tweet (note that even for RT'd tweets, it will be the person whose timeline it is)
+## time_posted - containing the date/time value that represents when the tweet was posted (note that this should be a TIMESTAMP column data type!)
+## tweet_text - containing the text that goes with that tweet
+## retweets - containing the number that represents how many times the tweet has been retweeted
+def setUpTweetTable(tweetList, conn, cur):
+
+	conn = sqlite3.connect("tweets.sqlite")
+    cur = conn.cursor()
+    cur.execute("DROP TABLE IF EXISTS Tweets")
+    cur.execute("CREATE TABLE Tweets(tweet_id REAL, author TEXT, time_posted TIMESTAMP, tweet_text TEXT, retweets REAL)")
+
+    for tweet in tweetList:
+        tweet_id = tweet["id"]
+        author = tweet["id"]
+        time_posted = tweet["id"]
+        tweet_text = tweet["id"]
+        retweets = tweet["id"]
+
+## [PART 3]
+# Finish the function getTimeAndText that returns a list of strings that contain the
+# time_posted and the text of the tweets.  It takes a database cursor as input.
+# Select the time_posted and tweet_text from the Tweets table in tweets.sqlite and return a list
+# of strings that contain the date/time and text of each tweet in the form: date/time - text as shown below
+# Mon Oct 09 16:02:03 +0000 2017 - #MondayMotivation https://t.co/vLbZpH390b
+def getTimeAndText(cur):
+
+	pass
+
+## [Part 4]
+# Finish the function getAuthorAndNumRetweets that returns a list of strings for the tweets that have been retweeted MORE than 2 times
+# It takes a database cursor as input.
+# Select the author (screen name) and number of retweets for of all of the tweets that have been retweeted MORE than 2 times
+# Return a list of strings that are in the form: author - # retweets as shown below
+# umsi - 5
+def getAuthorAndNumRetweets(cur):
+
+	pass
+
+## Unittests to test the functions
+class TestHW9(unittest.TestCase):
+    def setUp(self):
+        consumer_key = twitter_info.consumer_key
+        consumer_secret = twitter_info.consumer_secret
+        access_token = twitter_info.access_token
+        access_token_secret = twitter_info.access_token_secret
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token, access_token_secret)
+
+		# Set up library to grab stuff from twitter with your authentication, and return it in a JSON format
+        api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
+
+		# read the cache from the file if it exists
+        fname = "twitter_cache.json"
+        try:
+            cache_file = open(fname,'r')
+            cache_contents = cache_file.read()
+            cache_file.close()
+            cacheDict = json.loads(cache_contents)
+        except:
+            cacheDict = {}
+
+        self.conn = sqlite3.connect('tweets.sqlite')
+        self.cur = self.conn.cursor()
+        self.tweetList = get_tweets(api, cacheDict, fname)
+        setUpTweetTable(self.tweetList, self.conn, self.cur)
+
+    def test_setUpTweetTable(self):
+        self.cur.execute('SELECT * FROM Tweets')
+        self.assertEqual(35, len(self.cur.fetchall()))
+
+    def test_getTimeAndText(self):
+        strList = getTimeAndText(self.cur)
+        self.assertEqual(len(strList), 35)
+        self.assertEqual(strList[0], 'Tue Nov 13 22:02:48 +0000 2018 - Meet MSI student Huyen Phan and the team that created Peerstachio, the social network and learning community that a… https://t.co/Wyivz99Y73')
+
+    def test_getAuthorAndNumRetweets(self):
+        strList = getAuthorAndNumRetweets(self.cur)
+        self.assertEqual(len(strList), 6)
+        self.assertEqual(strList[0], 'umsi - 5')
+
+    def tearDown(self):
+        self.conn.close()
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
---
+++ README.md
@@ -0,0 +1,19 @@
+# HW9-Database
+In this homework you will read tweet information about "umsi" and from the user "umsi" from a cache file (or from Twitter if the cache isn't present) and then store it in a sqlite database. You will also write several functions to return information from the database. 
+
+- See HW-DB.py for more details.
+- See twitter_cache.json for the cache data that you will use for running the unittests.
+
+  
+
+Clone the project to your local computer 
+  - git clone projecturl
+- Edit HW-DB.py to add code and test with the unittests
+- Add the modified file to your list of files to commit and check the status
+  - git add HW-DB.py
+  - git status  
+- Be sure to do commits several times over the project. Do a commit each time that you get something working. You need to do at least 3 commits at least 4 hours apart to get the full credit for git commits unless you submit at least 24 hours before the deadline then the commits don't have to be 4 hours apart. 
+  - git commit -m "Explanation of what you have added"
+- Do a push to put your changes on your fork of this repo on github.  You don't have to do a pull request.  
+  - git push -u origin master
+- Upload a link to your repository on canvas to submit.
